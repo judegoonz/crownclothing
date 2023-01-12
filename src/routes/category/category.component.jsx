e82b38309f1c42d05c08bdc83214951be8711c66
Jude <el.caudillo.2484@gmail.com>
2023-01-12 21:03:28 +0800

    update

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -5,7 +5,7 @@ import ProductCard from '../../components/product-card/product-card.components';
 
 import { CategoriesContext } from '../../context/categories.context';
 
-import './category.styles.scss';
+import { CategoryContainer, CategoryTitle } from './category.styles';
 
 const Category = () => {
     const { category } = useParams();
@@ -19,13 +19,13 @@ const Category = () => {
 
     return (
         <Fragment>
-        <h2 className='category-title'>{category.toLocaleUpperCase()}</h2>
-        <div className='category-container'>
+        <CategoryTitle>{category.toLocaleUpperCase()}</CategoryTitle>
+        <CategoryContainer>
                 {products && 
                     products.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
-        </div>
+        </CategoryContainer>
         </Fragment>
     );
 };
